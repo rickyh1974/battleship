c3fddf08efc588537e5551c3cc5116c48ac2b92a
Ricky Hoben <rickyh1974@gmail.com>
2016-04-14 20:49:02 -0400

    test

--- tp2/src/main/java/jeu/Action.java
+++ tp2/src/main/java/jeu/Action.java
@@ -11,14 +11,30 @@ package jeu;
 public class Action {
     private static int numSeqAction=0;
     private Coordonnee point; // coordonnee x y donnee par joueur
+    private String nomJoeur; // contient nom du joueur
+    
 	/**
-	 * 
+	 * Constructor
 	 */
 	public Action() {
 		++numSeqAction;
 		 
 		// TODO Auto-generated constructor stub
 	}
+    
+	/**
+	 * @return the nomJoeur
+	 */
+	public String getNomJoeur() {
+		return nomJoeur;
+	}
+	/**
+	 * @param nomJoeur the nomJoeur to set
+	 */
+	public void setNomJoeur(String nomJoeur) {
+		this.nomJoeur = nomJoeur;
+	}
+
 	/**
 	 * @return the numSeqAction
 	 */
